e7f8dd90358dcb6b07b88fb96ec0e09be0cee0aa
jeenie <jeenie828@gmail.com>
2018-06-30 01:56:16 +0900

    Implement_Login_and_Logout

--- src/util/Gmail.java
+++ src/util/Gmail.java
@@ -7,6 +7,6 @@ public class Gmail extends Authenticator {
 
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new PasswordAuthentication("gmail_ID", "password");
+		return new PasswordAuthentication("google_Id", "password");
 	}
 }
